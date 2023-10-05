367a785a56b6b92807f378684f8789b297eedc1e
unknown <manikantapatel2805@gmail.com>
2023-10-05 17:33:50 +0530

    mani

---
+++ script1.js
@@ -0,0 +1,43 @@
+//    task-1
+// var number=0123456789
+// var a=+prompt("enter number")
+// a==number ? console.log("its a valid number") : alert("its not a valid number")
+
+//    task-2 
+// var a=199;
+// var b=900;
+// a > b ? console.log("a is greater than b"): console.log("b is greater than a");
+
+// var m=+prompt("enter first number")
+// var n=+prompt("enter second number")
+// m > n? console.log("m is greater than n"): console.log("n is greater than m")
+
+//   task-3
+// var value1=+prompt("enter first number")
+// var value2=+prompt("enter second number")
+// var value3=2+prompt("enter third number")
+// value1+value2+value3==60? console.log("Hello World"):console.log("Its not a 60");
+
+//    task-4
+// var day=+prompt("enter a number from 0 to 6");
+// day==0? console.log("sunday"):day==1? console.log("monday"):
+// day==2?console.log("tuesday"):day==3?console.log("wednesday"):
+// day==4? console.log("thursday"):day==5? console.log("friday"):
+// day==6?console.log("saturady"):console.log("not valid");
+
+
+//   task-5
+// var value=+prompt("enter a number");
+// value==12? console.log(++value):console.log(--value);
+
+//   task-6
+// var a1=prompt("enter a string");
+// a1=="manikanta"? console.log("chinthapanti"):console.log("not valid");
+
+//   task-750
+// var a=prompt("enter a number");
+// console.log(typeof(a));
+// a==="30"? console.log(a+30):console.log(a-30);
+
+
+
